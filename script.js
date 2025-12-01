b3b6d8b43d0f716fdae7750b27caa532ecf8799d
Константин <shkretov97@mail.ru>
2025-12-01 08:23:33 +0500

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+// выводит окошко на экран
+// alert('222')
+document.querySelector(".info__link").addEventListener("click", function () {
+    this.remove();
+    document.querySelector('.info__text').classList.add('info__text--active')
+});
